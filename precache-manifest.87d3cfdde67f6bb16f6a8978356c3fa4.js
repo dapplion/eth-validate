5c7d010505b2d3549bb7928b1b63d0c116c96562
lionbitrade <guillem.lion.benosa@gmail.com>
2018-12-04 23:07:33 +0800

    Updates

---
+++ precache-manifest.87d3cfdde67f6bb16f6a8978356c3fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "242e9e594c784f5c183e",
+    "url": "/eth-validate/static/js/runtime~main.242e9e59.js"
+  },
+  {
+    "revision": "4554e558ab3bcb4da83b",
+    "url": "/eth-validate/static/js/main.4554e558.chunk.js"
+  },
+  {
+    "revision": "11258cec66ad5fd91d57",
+    "url": "/eth-validate/static/js/1.11258cec.chunk.js"
+  },
+  {
+    "revision": "4554e558ab3bcb4da83b",
+    "url": "/eth-validate/static/css/main.072bf770.chunk.css"
+  },
+  {
+    "revision": "11258cec66ad5fd91d57",
+    "url": "/eth-validate/static/css/1.ee490de9.chunk.css"
+  },
+  {
+    "revision": "0749d5e3b7ca57e274e3a9b13b89f79f",
+    "url": "/eth-validate/index.html"
+  }
+];
